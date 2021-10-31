6996d5eceb62869b5e072940ac2e4a3f842fa9c1
amy-deng <sdeng4@stevens.edu>
2021-10-31 16:00:53 -0700

    construct doc-topic-word-causal-graph dgl testing

--- topic-src/models.py
+++ topic-src/models.py
@@ -23,52 +23,54 @@ except:
     pass
  
 
-# class HeteroLayerG(nn.Module):
-#     def __init__(self, in_size, out_size):
-#         super(HeteroLayerG, self).__init__()
-#         self.weight = nn.ModuleDict({
-#                 'ww': nn.Linear(in_size, out_size),
-#                 'wt': nn.Linear(out_size, out_size),
-#                 'wd': nn.Linear(out_size, out_size),
-#                 'td': nn.Linear(out_size, out_size),
-#                 'tt': nn.Linear(out_size, out_size),
-#             }) 
+class HeteroLayerG(nn.Module):
+    def __init__(self, in_size, out_size):
+        super(HeteroLayerG, self).__init__()
+        self.weight = nn.ModuleDict({
+                'ww': nn.Linear(in_size, out_size),
+                'wt': nn.Linear(out_size, out_size),
+                'wd': nn.Linear(out_size, out_size),
+                'td': nn.Linear(out_size, out_size),
+                'tt': nn.Linear(out_size, out_size),
+            }) 
 
-#     def forward(self, G):
-#         # print(G,feat_dict,'G,feat_dict')
-#         funcs={}
-#         for srctype, etype, dsttype in [['word','ww','word']]: 
-#             Wh = self.weight[etype](G.nodes['word'].data['h'])
-#             G.nodes[srctype].data['h'] = Wh
-#             funcs[etype] = (fn.u_mul_e('h', 'weight', 'm'), fn.mean('m', 'h'))
-#         G.multi_update_all(funcs, 'sum')
-#         funcs = {}
-#         # print(G.canonical_etypes)
-#         G.edges['tt'].data['weight'] = G.edges['tt'].data['weight'].float()
-#         for srctype, etype, dsttype in G.canonical_etypes:
-#             if etype == 'ww':
-#                 continue
-#             # print('srctype, etype, dsttype',srctype, etype, dsttype) 
-#             Wh = self.weight[etype](G.nodes[srctype].data['h'])
-#             G.nodes[srctype].data['h'] = Wh 
-#             funcs[etype] = (fn.u_mul_e('h', 'weight', 'm'), fn.mean('m', 'h')) 
-#         G.multi_update_all(funcs, 'sum')
-#         return G
-#         # return {ntype : G.nodes[ntype].data['h'] for ntype in G.ntypes}
+    def forward(self, G):
+        # print(G,feat_dict,'G,feat_dict')
+        funcs={}
+        for srctype, etype, dsttype in [['word','ww','word']]: 
+            Wh = self.weight[etype](G.nodes['word'].data['h'])
+            G.nodes[srctype].data['h'] = Wh
+            funcs[etype] = (fn.u_mul_e('h', 'weight', 'm'), fn.mean('m', 'h'))
+        G.multi_update_all(funcs, 'sum')
+        funcs = {}
+        # print(G.canonical_etypes)
+        G.edges['tt'].data['weight'] = G.edges['tt'].data['weight'].float()
+        for srctype, etype, dsttype in G.canonical_etypes:
+            if etype == 'ww':
+                continue
+            # print('srctype, etype, dsttype',srctype, etype, dsttype) 
+            Wh = self.weight[etype](G.nodes[srctype].data['h'])
+            G.nodes[srctype].data['h'] = Wh 
+            funcs[etype] = (fn.u_mul_e('h', 'weight', 'm'), fn.mean('m', 'h')) 
+        G.multi_update_all(funcs, 'sum')
+        return G
+        # return {ntype : G.nodes[ntype].data['h'] for ntype in G.ntypes}
 
-# class HeteroNetG(nn.Module):
-#     def __init__(self, in_size, hidden_size, out_size):
-#         super(HeteroNetG, self).__init__() 
-#         self.layer1 = HeteroLayerG(in_size, hidden_size)
-#         self.layer2 = HeteroLayerG(hidden_size, out_size)
+class HeteroNetG(nn.Module):
+    def __init__(self, in_size, hidden_size, out_size):
+        super(HeteroNetG, self).__init__() 
+        self.layer1 = HeteroLayerG(in_size, hidden_size)
+        self.layer2 = HeteroLayerG(hidden_size, out_size)
 
-#     def forward(self, G):
-#         h_dict = {ntype : G.nodes[ntype].data['h'] for ntype in G.ntypes}
-#         G = self.layer1(G, h_dict)
-#         h_dict = {ntype : G.nodes[ntype].data['h'] for ntype in G.ntypes}
-#         h_dict = {k : F.leaky_relu(h) for k, h in h_dict.items()}
-#         h_dict = self.layer2(G, h_dict)
-#         return G
+    def forward(self, G):
+        # h_dict = {ntype : G.nodes[ntype].data['h'] for ntype in G.ntypes}
+        G = self.layer1(G)
+        h_dict = {ntype : G.nodes[ntype].data['h'] for ntype in G.ntypes}
+        for ntype in h_dict:
+            G.nodes[ntype].data['h'] = F.leaky_relu(h_dict[ntype])
+        # h_dict = {k : F.leaky_relu(h) for k, h in h_dict.items()}
+        h_dict = self.layer2(G)
+        return G
 
 class HeteroLayer(nn.Module):
     def __init__(self, in_size, out_size):
@@ -217,6 +219,72 @@ class static_heto_graph(nn.Module):
         y_pred = torch.sigmoid(y_pred)
         return loss, y_pred
 
+class static_heto_graph2(nn.Module):
+    def __init__(self, h_inp, vocab_size, h_dim, device, seq_len=7, num_topic=50, num_word=15000,dropout=0.5):
+        super().__init__()
+        self.h_inp = h_inp
+        self.vocab_size = vocab_size
+        self.h_dim = h_dim
+        self.num_topic = num_topic
+        # self.num_rels = num_rels
+        self.seq_len = seq_len
+        self.device = device
+        self.dropout = nn.Dropout(dropout)
+        self.word_embeds = None
+        # initialize rel and ent embedding
+        # self.word_embeds = nn.Parameter(torch.Tensor(num_word, h_dim)) # change it to blocks
+        self.topic_embeds = nn.Parameter(torch.Tensor(num_topic, h_dim))
+        
+        self.hconv = HeteroNetG(h_inp, h_dim, h_dim)
+        # self.maxpooling  = nn.MaxPool1d(3)# 
+        # self.maxpooling  = dglnn.MaxPooling()
+        self.out_layer = nn.Linear(h_dim,1) 
+        self.threshold = 0.5
+        self.out_func = torch.sigmoid
+        self.criterion = F.binary_cross_entropy_with_logits #soft_cross_entropy
+        self.init_weights()
+
+
+    def init_weights(self):
+        for p in self.parameters():
+            if p.data.ndimension() >= 2:
+                nn.init.xavier_uniform_(p.data, gain=nn.init.calculate_gain('relu'))
+            else:
+                stdv = 1. / math.sqrt(p.size(0))
+                p.data.uniform_(-stdv, stdv)
+
+    def forward(self, g_list, y_data): 
+        bg = dgl.batch(g_list).to(self.device) 
+        word_emb = self.word_embeds[bg.nodes['word'].data['id']].view(-1, self.word_embeds.shape[1])
+        topic_emb = self.topic_embeds[bg.nodes['topic'].data['id']].view(-1, self.topic_embeds.shape[1])
+        doc_emb = torch.zeros((bg.number_of_nodes('doc'), self.h_dim)).to(self.device)
+        bg.nodes['word'].data['h'] = word_emb
+        bg.nodes['topic'].data['h'] = topic_emb
+        bg.nodes['doc'].data['h'] = doc_emb
+         
+        bg = self.hconv(bg)
+        doc_emb = bg.nodes['doc'].data['h']
+        doc_len = [g.num_nodes('doc') for g in g_list]
+        doc_emb_split = torch.split(doc_emb, doc_len)
+        # print(len(doc_emb_split),'doc_emb_split',doc_emb_split[0].shape)
+        # padding to same size  
+        # print(max(doc_len),'max(doc_len)')
+        # embed_pad_tensor = torch.zeros(len(doc_len), max(doc_len), self.h_dim).to(self.device)
+        mean_embed = torch.zeros(len(doc_len), self.h_dim).to(self.device)
+        for i, embeds in enumerate(doc_emb_split): 
+            mean_embed[i, :] = embeds.mean(0)
+        # for i, embeds in enumerate(doc_emb_split): 
+        #         embed_pad_tensor[i, torch.arange(0,len(embeds)), :] = embeds
+
+        # doc_pool = embed_pad_tensor.mean(1)
+        # doc_pool = self.maxpooling(embed_pad_tensor)
+        # print(doc_pool.shape,'doc_pool')
+        # doc_emb_mean = doc_emb.mean(0)
+        y_pred = self.out_layer(mean_embed)
+        # print(y_pred.shape,'y_pred',y_pred,y_data.shape,'y_data')
+        loss = self.criterion(y_pred.view(-1), y_data)
+        y_pred = torch.sigmoid(y_pred)
+        return loss, y_pred
 
 class static_graph(nn.Module):
     def __init__(self, h_inp, vocab_size, h_dim, device, seq_len=7, num_topic=50, num_word=15000,dropout=0.5):
