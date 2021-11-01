a1d62688a3e80bacba990f6a96acb5dee85524ca
amy-deng <sdeng4@stevens.edu>
2021-11-01 00:06:01 -0700

    construct doc-topic-word-causal-graph dgl testing

--- topic-src/train.py
+++ topic-src/train.py
@@ -112,9 +112,9 @@ test_loader.len = len(test_indices)
 def prepare(args,word_embeds,device): 
     if args.model == 'm0':
         model = static_heto_graph(h_inp=emb_size, vocab_size=vocab_size, h_dim=args.n_hidden, device=device)
-    elif args.model == 'm2':
+    elif args.model == 'uni':
         model = static_heto_graph_causal_uni(h_inp=emb_size, vocab_size=vocab_size, h_dim=args.n_hidden, device=device)
-    elif args.model == 'm3':
+    elif args.model == 'cus':
         model = static_heto_graph_causal_cus(h_inp=emb_size, vocab_size=vocab_size, h_dim=args.n_hidden, device=device)
     elif args.model == 'm1':
         model = static_heto_graph0(h_inp=emb_size, vocab_size=vocab_size, h_dim=args.n_hidden, device=device)
