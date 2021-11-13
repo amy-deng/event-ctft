825d23d1adfce1a1ef03767dea6a8d6a0dc0b180
amy-deng <sdeng4@stevens.edu>
2021-11-12 21:00:11 -0800

    .

--- topic-src/model_gcn_hetero.py
+++ topic-src/model_gcn_hetero.py
@@ -60,7 +60,7 @@ class GCNLayer(nn.Module):
         return '{}(in_dim={}, out_dim={})'.format(
             self.__class__.__name__, self.in_feats, self.out_feats)
 
-
+'''
 class LevelHeteroConvLayer(nn.Module):
     def __init__(self, in_feats, out_feats, activation, dropout):
         super().__init__()
@@ -103,6 +103,47 @@ class LevelHeteroConvLayer(nn.Module):
 
         # return G.nodes['doc'].data['h']
         return {ntype : G.nodes[ntype].data['h'] for ntype in G.ntypes}
+'''
+
+class HeteroConvLayer(nn.Module):
+    def __init__(self, in_feats, out_feats, activation, dropout, etypes):
+        super().__init__()
+        self.etypes = etypes
+        self.weight = nn.ModuleDict()
+        for etype in etypes:
+            self.weight[etype] = nn.Linear(in_feats, out_feats)
+        self.activation = activation
+        self.drop = nn.Dropout(dropout)
+
+    def forward(self, G, feat_dict):
+        for ntype in feat_dict:
+            G.nodes[ntype].data['h'] = feat_dict[ntype]
+        # print(G,feat_dict,'G,feat_dict')
+        funcs={}
+        # G.edges['tt'].data['weight'] = G.edges['tt'].data['weight'].float()
+        G.edges['wd'].data['weight'] = G.edges['wd'].data['weight'].float()
+        for srctype, etype, dsttype in G.canonical_etypes:
+            if etype not in self.etypes:
+                continue
+            # print('srctype, etype, dsttype',srctype, etype, dsttype,feat_dict[srctype].shape) 
+            Wh = self.weight[etype](G.nodes[srctype].data['h'])   #   feat_dict[srctype]
+            G.nodes[srctype].data['Wh_%s' % etype] = Wh
+            # print(etype,G.edges[etype].data['weight'].dtype,Wh.dtype)
+            funcs[etype] = (fn.u_mul_e('Wh_%s' % etype, 'weight', 'm'), fn.mean('m', 'h'))
+
+        G.multi_update_all(funcs, 'sum')
+
+        for ntype in G.ntypes:
+            # if ntype != 'doc':
+            #     continue
+            # n_id = node_dict[ntype]
+            # print(self.skip[ntype],'self.skip[ntype]')
+            feat = G.nodes[ntype].data['h']
+            G.nodes[ntype].data['h'] = self.drop(self.activation(feat))
+
+        # return G.nodes['doc'].data['h']
+        return {ntype : G.nodes[ntype].data['h'] for ntype in G.ntypes}
+
 
 
 class GCNHet(nn.Module):
@@ -134,7 +175,7 @@ class GCNHet(nn.Module):
             self.gcn_topic_layers.append(GCNLayer(n_hid, n_hid, activation, dropout))
         self.hetero_layers = nn.ModuleList()
         for _ in range(n_layers):
-            self.hetero_layers.append(LevelHeteroConvLayer(n_hid, n_hid, activation, dropout))
+            self.hetero_layers.append(HeteroConvLayer(n_hid, n_hid, activation, dropout, ['wt','wd','td']))
         self.out_layer = nn.Linear(n_hid, 1)  
         self.threshold = 0.5
         self.out_func = torch.sigmoid
@@ -213,7 +254,7 @@ class GCNHetAll(nn.Module):
             self.gcn_topic_layers.append(GCNLayer(n_hid, n_hid, activation, dropout))
         self.hetero_layers = nn.ModuleList()
         for _ in range(n_layers):
-            self.hetero_layers.append(LevelHeteroConvLayer(n_hid, n_hid, activation, dropout))
+            self.hetero_layers.append(HeteroConvLayer(n_hid, n_hid, activation, dropout, ['wt','wd','td']))
         self.out_layer = nn.Sequential(
                 nn.Linear(n_hid*3, n_hid),
                 nn.BatchNorm1d(n_hid),
@@ -303,7 +344,7 @@ class GCNHetAll2(nn.Module):
             self.gcn_topic_layers.append(GCNLayer(n_hid, n_hid, activation, dropout))
         self.hetero_layers = nn.ModuleList()
         for _ in range(n_layers):
-            self.hetero_layers.append(LevelHeteroConvLayer(n_hid, n_hid, activation, dropout))
+            self.hetero_layers.append(HeteroConvLayer(n_hid, n_hid, activation, dropout, ['wt','wd','td']))
         self.out_layer = nn.Sequential(
                 nn.Linear(n_hid*3, n_hid),
                 nn.BatchNorm1d(n_hid),
@@ -335,6 +376,84 @@ class GCNHetAll2(nn.Module):
         for i in range(len(self.gcn_word_layers)):
             word_emb = self.gcn_word_layers[i](bg, word_emb, 'word','ww')
             topic_emb = self.gcn_topic_layers[i](bg, topic_emb, 'topic','tt')
+            feat_dict = {'word':word_emb, 'topic':topic_emb}
+            feat_dict = self.hetero_layers[i](bg, feat_dict)
+        
+        if self.pool == 'max':
+            global_doc_info = dgl.max_nodes(bg, feat='h',ntype='doc')
+            global_word_info = dgl.max_nodes(bg, feat='h',ntype='word')
+            global_topic_info = dgl.max_nodes(bg, feat='h',ntype='topic')
+        elif self.pool == 'mean':
+            global_doc_info = dgl.mean_nodes(bg, feat='h',ntype='doc')
+            global_word_info = dgl.mean_nodes(bg, feat='h',ntype='word')
+            global_topic_info = dgl.mean_nodes(bg, feat='h',ntype='topic')
+        # print(global_doc_info,'global_doc_info')
+        global_info = torch.cat((global_doc_info, global_word_info, global_topic_info),-1)
+        y_pred = self.out_layer(global_info)
+        # print(y_pred.shape,'y_pred',y_pred,y_data.shape,'y_data')
+        loss = self.criterion(y_pred.view(-1), y_data)
+        y_pred = torch.sigmoid(y_pred)
+        return loss, y_pred
+
+
+class GCNTopicAll(nn.Module):
+    def __init__(self, n_inp, n_hid, n_layers, activation, device, num_topic=50, vocab_size=15000, dropout=0.5, pool='max', use_norm = True):
+        super(GCNTopicAll, self).__init__()
+        self.n_inp = n_inp
+        self.n_hid = n_hid
+        self.n_layers = n_layers
+        self.vocab_size = vocab_size
+        self.num_topic = num_topic
+        self.device = device
+        self.pool = pool
+        self.dropout = nn.Dropout(dropout)
+        self.activation = activation
+        self.word_embeds = None
+        # initialize rel and ent embedding
+        # self.word_embeds = nn.Parameter(torch.Tensor(num_word, h_dim)) # change it to blocks
+        self.topic_embeds = nn.Parameter(torch.Tensor(num_topic, n_hid))
+        # self.doc_gen_embeds = nn.Parameter(torch.Tensor(1,n_hid))
+        self.gcn_word_layers = nn.ModuleList()
+        self.gcn_word_layers.append(GCNLayer(n_inp, n_hid, activation, dropout))
+        for _ in range(n_layers-1):
+            self.gcn_word_layers.append(GCNLayer(n_hid, n_hid, activation, dropout))
+        # self.gcn_topic_layers = nn.ModuleList()
+        # for _ in range(n_layers):
+        #     self.gcn_topic_layers.append(GCNLayer(n_hid, n_hid, activation, dropout))
+        self.hetero_layers = nn.ModuleList()
+        for _ in range(n_layers):
+            self.hetero_layers.append(HeteroConvLayer(n_hid, n_hid, activation, dropout, ['wt','tt','wd','td']))
+        self.out_layer = nn.Sequential(
+                nn.Linear(n_hid*3, n_hid),
+                nn.BatchNorm1d(n_hid),
+                nn.Linear(n_hid, 1) 
+        )
+         
+        self.threshold = 0.5
+        self.out_func = torch.sigmoid
+        self.criterion = F.binary_cross_entropy_with_logits #soft_cross_entropy
+        self.init_weights()
+
+    def init_weights(self):
+        for p in self.parameters():
+            if p.data.ndimension() >= 2:
+                nn.init.xavier_uniform_(p.data, gain=nn.init.calculate_gain('relu'))
+            else:
+                stdv = 1. / math.sqrt(p.size(0))
+                p.data.uniform_(-stdv, stdv)
+
+    def forward(self, g_list, y_data): 
+        bg = dgl.batch(g_list).to(self.device) 
+        
+        word_emb = self.word_embeds[bg.nodes['word'].data['id']].view(-1, self.word_embeds.shape[1])
+        topic_emb = self.topic_embeds[bg.nodes['topic'].data['id']].view(-1, self.topic_embeds.shape[1])
+        # doc_emb = self.doc_gen_embeds.repeat(bg.number_of_nodes('doc'),1)
+        # torch.zeros((bg.number_of_nodes('doc'), self.n_hid)).to(self.device)
+        # for i in range(len(self.gcn_topic_layers)):
+        # print('word_emb',word_emb.shape,topic_emb.shape,'doc_emb',doc_emb.shape)
+        for i in range(len(self.gcn_word_layers)):
+            word_emb = self.gcn_word_layers[i](bg, word_emb, 'word','ww')
+            # topic_emb = self.gcn_topic_layers[i](bg, topic_emb, 'topic','tt')
             feat_dict = {'word':word_emb, 'topic':topic_emb}
             feat_dict = self.hetero_layers[i](bg, feat_dict)
         
