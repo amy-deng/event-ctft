15d2033924efe26f3498775846e184ced564a8d8
amy-deng <sdeng4@stevens.edu>
2021-11-15 20:35:24 -0800

    add build_hetero_graph_time.py.

--- topic-src/build_causal_samples_w_time_nocheck.py
+++ topic-src/build_causal_samples_w_time_nocheck.py
@@ -11,8 +11,8 @@ from text_utils import *
 from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
 
 '''
-python build_causal_samples_w_time_nocheck.py ../data THA_topic check_topic_causal_data_w7h7
-python build_causal_samples_w_time_nocheck.py ../data THA_topic check_topic_causal_data_w7h14
+python build_causal_samples_w_time_new.py ../data THA_topic check_topic_causal_data_w7h7
+python build_causal_samples_w_time_new.py ../data THA_topic check_topic_causal_data_w7h14
 
 '''
 try:
@@ -21,8 +21,9 @@ try:
     raw_data_name = sys.argv[3]  
     start_date = sys.argv[4]
     end_date = sys.argv[5]
+    check = int(sys.argv[6])
 except:
-    print("usage: <out_path> <dataset `THA_topic`> <raw_data_name `raw_w10h7`> <start_date> <end_date>")
+    print("usage: <out_path> <dataset `THA_topic`> <raw_data_name `raw_w10h7`> <start_date> <end_date> <no_check check 1/0>")
     exit()
 
 
@@ -40,11 +41,19 @@ dates = data['date'] # np.array
 
  
 # 2017-01-01
+# splitted_date_lists = [
+#     '2010-07-01',
+#     '2011-01-01','2011-07-01','2012-01-01','2012-07-01','2013-01-01','2013-07-01',
+#     '2014-01-01','2014-07-01','2015-01-01','2015-07-01','2016-01-01','2016-07-01',
+#     '2017-01-01','2017-07-01'
+# ]
+
 splitted_date_lists = [
-    '2010-07-01',
-    '2011-01-01','2011-07-01','2012-01-01','2012-07-01','2013-01-01','2013-07-01',
-    '2014-01-01','2014-07-01','2015-01-01','2015-07-01','2016-01-01','2016-07-01',
-    '2017-01-01','2017-07-01'
+    '2013-01-01','2013-04-01','2013-07-01','2013-10-01',
+    '2014-01-01','2014-04-01','2014-07-01','2014-10-01',
+    '2015-01-01','2015-04-01','2015-07-01','2015-10-01',
+    '2016-01-01','2016-04-01','2016-07-01','2016-10-01',
+    '2017-01-01','2017-04-01'
 ]
 
 
@@ -67,40 +76,83 @@ def save_samples(treatment, outcome, covariate, outpath):
                     'outcome':outcome},f)
     return
 
-sorted_indices = np.argsort(dates)
+# sorted_indices = np.argsort(dates)
 
 samples_by_time = {}
+
+# save samples for each time intervals
 
 for topic_id in range(50):
     treatment_assign_by_topic = []
     covariate_by_topic = []
     outcome_by_topic = []
 
-    cur_time_split_idx = 0
-    for i in sorted_indices:
+    for t in range(1,len(splitted_date_lists)):
+        start = splitted_date_lists[i-1]
+        end = splitted_date_lists[i]
+        
+        treatment_assign_by_topic = []
+        covariate_by_topic = []
+        outcome_by_topic = []
+        for i in range(len(dates)):
+            if dates[i] >= start and dates[i] < end:
+                curr_treament = treatment[i]
+                past_treatment = treatment_check[i]
+                if check == 0:
+                    if curr_treament[topic_id] > 0: # treated sample
+                        treatment_assign_by_topic.append(1)
+                    else: # control sample
+                        treatment_assign_by_topic.append(0)
+                    covariate_by_topic.append(covariate[i])
+                    outcome_by_topic.append(outcome[i])
+                else:
+                    if past_treatment[topic_id] <= 0:
+                        if curr_treament[topic_id] > 0: # treated sample
+                            treatment_assign_by_topic.append(1)
+                        else: # control sample
+                            treatment_assign_by_topic.append(0)
+                        covariate_by_topic.append(covariate[i])
+                        outcome_by_topic.append(outcome[i])
+                    else:
+                        pass
+        print(start,'-',end, len(treatment_assign_by_topic),'treatment_assign_by_topic')
+        if check == 0: 
+            out_path = "{}/nocheck_topic_{}_{}_{}_{}.pkl".format(save_path,topic_id,start,end)
+        else:
+            out_path = "{}/check_topic_{}_{}_{}_{}.pkl".format(save_path,topic_id,start,end)
+        save_samples(treatment_assign_by_topic,outcome_by_topic,covariate_by_topic,out_path)
 
-        date = dates[i]
-        if date < start_date or date >= start_date:
-            continue
-        if date > splitted_date_lists[cur_time_split_idx]:
+#         pass
+# for topic_id in range(50):
+#     treatment_assign_by_topic = []
+#     covariate_by_topic = []
+#     outcome_by_topic = []
+
+#     cur_time_split_idx = 0
+#     for i in sorted_indices:
+
+#         date = dates[i]
+#         if date < start_date or date >= start_date:
+#             continue
+#         if date > splitted_date_lists[cur_time_split_idx]:
             
-            out_path = "{}/nocheck_topic_{}_{}.pkl".format(save_path,topic_id,splitted_date_lists[cur_time_split_idx])
-            save_samples(treatment_assign_by_topic,outcome_by_topic,covariate_by_topic,out_path)
-            cur_time_split_idx += 1
+#             out_path = "{}/nocheck_topic_{}_{}.pkl".format(save_path,topic_id,splitted_date_lists[cur_time_split_idx])
+#             save_samples(treatment_assign_by_topic,outcome_by_topic,covariate_by_topic,out_path)
+#             cur_time_split_idx += 1
             
-        curr_treament = treatment[i]
-        past_treatment = treatment_check[i]
-        # if past_treatment[topic_id] <= 0:
-        if curr_treament[topic_id] > 0: # treated sample
-            treatment_assign_by_topic.append(1)
-        else: # control sample
-            treatment_assign_by_topic.append(0)
-        covariate_by_topic.append(covariate[i])
-        outcome_by_topic.append(outcome[i])
-        # else:
-        #     pass
+#         curr_treament = treatment[i]
+#         past_treatment = treatment_check[i]
+#         # if past_treatment[topic_id] <= 0:
+#         if curr_treament[topic_id] > 0: # treated sample
+#             treatment_assign_by_topic.append(1)
+#         else: # control sample
+#             treatment_assign_by_topic.append(0)
+#         covariate_by_topic.append(covariate[i])
+#         outcome_by_topic.append(outcome[i])
+#         # else:
+#         #     pass
     
-    out_path = "{}/nocheck_topic_{}_{}_{}_{}.pkl".format(save_path,topic_id,splitted_date_lists[cur_time_split_idx],start_date, end_date)
-    save_samples(treatment_assign_by_topic,outcome_by_topic,covariate_by_topic,out_path)
+#     out_path = "{}/nocheck_topic_{}_{}_{}_{}.pkl".format(save_path,topic_id,start_date, end_date)
+#     save_samples(treatment_assign_by_topic,outcome_by_topic,covariate_by_topic,out_path)
 
         
