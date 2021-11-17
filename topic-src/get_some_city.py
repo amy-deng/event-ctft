cf6481a109c80414441bc07648a02e01958822f3
amy-deng <sdeng4@stevens.edu>
2021-11-16 16:10:00 -0800

    .

--- topic-src/get_some_city.py
+++ topic-src/get_some_city.py
@@ -1,15 +1,77 @@
 import pandas as pd
 
 import pickle
+import numpy as np
 
-with open('../RUS_w7h7_minday7/hetero_2014-2015_1000.pkl','rb') as f:
+
+with open('../EGY_w7h7_minday7/hetero_2015-2016_1000.pkl','rb') as f:
     d1=pickle.load(f)
 
-with open('../RUS_w7h7_minday7/hetero_2015-2016_1000.pkl','rb') as f:
+
+
+with open('../EGY_w7h7_minday7/static_tf_2016-2017_800.pkl','rb') as f:
+    static14 = pickle.load(f)
+#
+with open('../EGY_w7h7_minday7/dynamic_tf_2016-2017_800.pkl','rb') as f:
+    dynamic14 = pickle.load(f)
+with open('../EGY_w7h7_minday7/attr_tf_2016-2017_800.pkl','rb') as f:
+    attr14 = pickle.load(f)
+
+city = attr14['city']
+y = attr14['y']
+date = attr14['date']
+print('y',type(y),y.shape)
+print('date',type(date),len(date))
+print('city',type(city),len(city))
+
+new_static14 = []
+new_dynamic14 = []
+new_attr = []
+new_y = []
+new_date = []
+new_city = []
+c =0
+
+for i in range(len(static14)):
+    static = static14[i]
+    num_doc = static.num_nodes('doc')
+    if num_doc <= 10:
+        c+=1
+    else:
+        new_static14.append(static)
+        new_dynamic14.append(dynamic14[i])
+        new_city.append(city[i])
+        new_date.append(date[i])
+        new_y.append(y[i])
+
+print('new_date',type(new_date),len(new_date))
+print('new_city',type(new_city),len(new_city))
+new_y = np.stack(new_y,0)
+new_attr14 = {'y':new_y,'date':new_date,'city':new_city}
+
+with open('../RUS_w7h7_minday10/static_tf_2016-2017_800.pkl','wb') as f:
+    pickle.dump(new_static14,f)
+#
+with open('../RUS_w7h7_minday10/dynamic_tf_2016-2017_800.pkl','wb') as f:
+    pickle.dump(new_dynamic14,f)
+
+with open('../RUS_w7h7_minday10/attr_tf_2016-2017_800.pkl','wb') as f:
+    pickle.dump(new_attr14,f)
+
+
+
+
+
+
+
+with open('../EGY_w7h7_minday7/hetero_2014-2015_1000.pkl','rb') as f:
+    d1=pickle.load(f)
+
+with open('../EGY_w7h7_minday7/hetero_2015-2016_1000.pkl','rb') as f:
     d2=pickle.load(f)
 
 
-with open('../RUS_w7h7_minday7/hetero_2016-2017_1000.pkl','rb') as f:
+with open('../EGY_w7h7_minday7/hetero_2016-2017_1000.pkl','rb') as f:
     d3=pickle.load(f)
 
 city1 = d1['city']
