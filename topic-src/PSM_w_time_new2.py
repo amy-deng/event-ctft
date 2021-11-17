4c9a4dd6293174a752897b5fecb4673e943c1257
amy-deng <sdeng4@stevens.edu>
2021-11-17 09:12:49 -0800

    propensity score method testing

--- topic-src/PSM_w_time_new2.py
+++ topic-src/PSM_w_time_new2.py
@@ -146,7 +146,8 @@ for file in file_list:
     criterion = nn.BCEWithLogitsLoss()
     optm = torch.optim.Adam(net.parameters(), lr=0.001, weight_decay=1e-5)
     X_torch = torch.from_numpy(X).float()
-    y_torch = torch.from_numpy(treatment).long()
+    y_torch = torch.from_numpy(treatment).int()
+    print(X_torch.dtype(),'X_torch',y_torch.dtype())
     our_dataset = OurDataset(X_torch,y_torch)
     train_dataloader = DataLoader(our_dataset, batch_size=BATCH_SIZE, shuffle=True)
     for epoch in range(EPOCHS):
