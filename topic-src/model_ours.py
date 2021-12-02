ca842fefe8ff95fc7ccd540627a49eb3e4182881
amy-deng <sdeng4@stevens.edu>
2021-12-02 00:03:29 -0800

    temp21

--- topic-src/model_ours.py
+++ topic-src/model_ours.py
@@ -1672,6 +1672,7 @@ class Temp21(nn.Module):
             causal_w = self.cau_weight[curr_time][topic_ids]
             # effect = sub_bg.nodes['topic'].data['effect'].to_dense()
             # print('causal_w',causal_w.shape,'cau_weight',self.cau_weight.shape,'topic_ids',topic_ids.shape)
+            print(causal_w.type(),effect.type(),self.cau_embeds.type())
             t = (effect.double() * causal_w) @ self.cau_embeds 
             # print('t',t.shape)
 
