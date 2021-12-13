47f44a82a5457ca806837d68057b1905434c7b6e
amy-deng <sdeng4@stevens.edu>
2021-12-12 23:20:45 -0800

    temp61

--- topic-src/train.py
+++ topic-src/train.py
@@ -176,6 +176,9 @@ def prepare(args,word_embeds,device):
     elif args.model == 'temp6':
         model = Temp6(n_inp=emb_size, n_hid=args.n_hidden, n_layers=args.n_layers, n_heads=args.n_heads, activation=F.relu, seq_len=args.seq_len,device=device, 
         num_topic=args.n_topics, vocab_size=vocab_size, dropout=args.dropout,pool=args.pool, use_norm = True)
+    elif args.model == 'temp61':
+        model = Temp61(n_inp=emb_size, n_hid=args.n_hidden, n_layers=args.n_layers, n_heads=args.n_heads, activation=F.relu, seq_len=args.seq_len,device=device, 
+        num_topic=args.n_topics, vocab_size=vocab_size, dropout=args.dropout,pool=args.pool, use_norm = True)
 
 
     # elif args.model == 'cau6':
