255122f0cad219f422bf2bb65b7a06e145756262
amy-deng <sdeng4@stevens.edu>
2021-12-12 22:47:52 -0800

    .

--- topic-src/main.py
+++ topic-src/main.py
@@ -20,7 +20,7 @@ except:
     exit()
 available_models = ['gcn','gat','heteroall','gcnetall3','gcnetall4','hgtall','hanall','rgcnall','temphgtall',\
     'ours','hgtallcau','ours2','ours3','ours4','ours5','ours5rgcn','ours6','cau6','rgcn','hgt','temphgt',\
-        'temp1','temp11','temp2','temp21','temp3','temp4','temp41','temp5']
+        'temp1','temp11','temp2','temp21','temp3','temp4','temp41','temp5','temp6']
 
 model_l = models.split(',')
 for m in model_l:
--- topic-src/train.py
+++ topic-src/train.py
@@ -173,6 +173,9 @@ def prepare(args,word_embeds,device):
     elif args.model == 'temp5':
         model = Temp5(n_inp=emb_size, n_hid=args.n_hidden, n_layers=args.n_layers, n_heads=args.n_heads, activation=F.relu, seq_len=args.seq_len,device=device, 
         num_topic=args.n_topics, vocab_size=vocab_size, dropout=args.dropout,pool=args.pool, use_norm = True)
+    elif args.model == 'temp6':
+        model = Temp6(n_inp=emb_size, n_hid=args.n_hidden, n_layers=args.n_layers, n_heads=args.n_heads, activation=F.relu, seq_len=args.seq_len,device=device, 
+        num_topic=args.n_topics, vocab_size=vocab_size, dropout=args.dropout,pool=args.pool, use_norm = True)
 
 
     # elif args.model == 'cau6':
