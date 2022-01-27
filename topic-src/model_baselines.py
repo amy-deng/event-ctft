22b265d4939ff9c682271355c50637bc96870f7c
amy-deng <sdeng4@stevens.edu>
2022-01-27 08:43:28 -0800

    dyngcn

--- topic-src/model_baselines.py
+++ topic-src/model_baselines.py
@@ -781,7 +781,7 @@ class dyngcn(nn.Module):
             cat_h = torch.cat((h,h0),dim=-1)
             
             cat_h = self.dropout(self.bn(cat_h))
-            h = torch.relu(self.temp_encoding(cat_h))+h
+            h = torch.tanh(self.temp_encoding(cat_h))
             # h = self.layers[curr_time](sub_bg, h, ntype='word',etype='ww') 
             for layer in self.layers:
                 h = layer(sub_bg, h, ntype='word',etype='ww') 
