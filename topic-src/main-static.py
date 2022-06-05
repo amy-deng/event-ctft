f80515d28c935cecc7f350a4209d4d984655f153
amy-deng <sdeng4@stevens.edu>
2022-06-05 10:17:01 -0400

    evolve gcn

--- topic-src/main-static.py
+++ topic-src/main-static.py
@@ -4,10 +4,10 @@ import numpy as np
 from subprocess import call
 
 '''
-python main-static.py gcn,gat,rgcn EGY_w7h7_mind3n7df0.01 sta_tf_2014-2015_900,sta_tf_2015-2016_900,sta_tf_2016-2017_900 5 64 3 4  
-python main-static.py gcn,gat,rgcn AFG_w7h7_mind3n7df0.01 sta_tf_2014-2015_900,sta_tf_2015-2016_900,sta_tf_2016-2017_900 5 64 3 4  
-python main-static.py gcn,gat,rgcn RUS_w7h7_mind3n10df0.01 sta_tf_2014-2015_900,sta_tf_2015-2016_900,sta_tf_2016-2017_900 5 64 3 4  
-python main-static.py gcn,gat,rgcn THA_w7h7_mind3n7df0.01 sta_tf_2014-2015_900,sta_tf_2015-2016_900,sta_tf_2016-2017_900 5 64 3 4  
+python main-static.py gcn,gat,rgcn EGY_w7h7_mind3n7df0.01 sta_tf_2014-2015_900,sta_tf_2015-2016_900,sta_tf_2016-2017_900 5 64 3 0  "--train 0.4"
+python main-static.py gcn,gat,rgcn AFG_w7h7_mind3n7df0.01 sta_tf_2014-2015_900,sta_tf_2015-2016_900,sta_tf_2016-2017_900 5 64 3 6    "--train 0.4"
+python main-static.py gcn,gat,rgcn RUS_w7h7_mind3n10df0.01 sta_tf_2014-2015_900,sta_tf_2015-2016_900,sta_tf_2016-2017_900 5 64 3 8    "--train 0.4"
+python main-static.py gcn,gat,rgcn THA_w7h7_mind3n7df0.01 sta_tf_2014-2015_900,sta_tf_2015-2016_900,sta_tf_2016-2017_900 5 64 3 7    "--train 0.4"
 
 '''
 
@@ -20,9 +20,9 @@ try:
     layer = sys.argv[6]
     # note = sys.argv[7]
     gpu = sys.argv[7]
-    # others = sys.argv[9]
+    others = sys.argv[8]
 except:
-    print('Usage: models, datasets, datafiles, horizon, hidden, layer, gpu')
+    print('Usage: models, datasets, datafiles, horizon, hidden, layer, gpu, others')
     exit()
 available_models = ['gcn','gat','rgcn']
 
@@ -36,7 +36,7 @@ dataset_l = datasets.split(',')
 for mod in model_l:
     for dataset in dataset_l:
     # for horizon in horizon_l:
-        command = "python train.py --dataset {} --datafiles {} --horizon {} --gpu {} -m {}  --n-hidden {} --n-layers {} ".format(\
-        dataset,datafiles,horizon,gpu,mod,hidden,layer)  
+        command = "python train.py --dataset {} --datafiles {} --horizon {} --gpu {} -m {}  --n-hidden {} --n-layers {} {}".format(\
+        dataset,datafiles,horizon,gpu,mod,hidden,layer,others)  
     print(command)
     call(command, shell=True)
